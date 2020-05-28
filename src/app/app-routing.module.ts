ca01c60a99b48c28245af2070ab12844ea018ece
Mohamed Sabthar <sabtharugc0@gmail.com>
2020-05-28 20:33:11 +0530

    Angular file structure created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,29 @@
+import { AdminComponent } from './pages/main-content/admin/admin.component';
+import { DriverManagementComponent } from './pages/main-content/admin/driver-management/driver-management.component';
+import { AdminDashboardComponent } from './pages/main-content/admin/admin-dashboard/admin-dashboard.component';
+import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
+import { ServerErrorPageComponent } from './pages/server-error-page/server-error-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+const routes: Routes = [
+  { path: '', component: LoginPageComponent },
+  { path: 'server-error', component: ServerErrorPageComponent },
+  {
+    path: 'admin',
+    component: AdminComponent,
+    children: [
+      { path: 'dashboard', component: AdminDashboardComponent },
+      { path: 'driver-management', component: DriverManagementComponent },
+    ],
+  },
 
-const routes: Routes = [];
+  { path: '**', component: NotFoundPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
+import { AdminDashboardComponent } from './pages/main-content/admin/admin-dashboard/admin-dashboard.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
+import { ServerErrorPageComponent } from './pages/server-error-page/server-error-page.component';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { DriverManagementComponent } from './pages/main-content/admin/driver-management/driver-management.component';
+import { AdminComponent } from './pages/main-content/admin/admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    NotFoundPageComponent,
+    ServerErrorPageComponent,
+    SideBarComponent,
+    AdminDashboardComponent,
+    NavBarComponent,
+    DriverManagementComponent,
+    AdminComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
