c8394a4822e4a153bf64e0dc2d1bb49da7bdc9d6
Faizceo <mohommed.faiz@gmail.com>
2020-06-17 01:25:10 +0530

    --allow-empty-message

--- src/app/api.constants.ts
+++ src/app/api.constants.ts
@@ -1,7 +1,7 @@
 export class API {
-  public static base :string = "http://localhost:3000"
+  public static base: string = 'https://smart-cargo.herokuapp.com';
 
-  public static login(){
+  public static login() {
     return `${API.base}/auth/sign-in`;
   }
 }
