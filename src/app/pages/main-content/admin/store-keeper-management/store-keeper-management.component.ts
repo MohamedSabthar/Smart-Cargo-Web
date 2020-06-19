690846f8121e46c67a85d226676239959743796c
DivaneeSiva <36619865+DivaneeSiva@users.noreply.github.com>
2020-06-20 01:15:00 +0530

    driver-management and store-keeper management hardcoded UI

--- src/app/pages/main-content/admin/store-keeper-management/store-keeper-management.component.ts
+++ src/app/pages/main-content/admin/store-keeper-management/store-keeper-management.component.ts
@@ -1,13 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import {NgbNavConfig} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-store-keeper-management',
   templateUrl: './store-keeper-management.component.html',
-  styleUrls: ['./store-keeper-management.component.css']
+  styleUrls: ['./store-keeper-management.component.css'],
+  providers: [NgbNavConfig]
 })
 export class StoreKeeperManagementComponent implements OnInit {
 
-  constructor() { }
+  constructor(config: NgbNavConfig) {
+    // customize default values of navs used by this component tree
+    config.destroyOnHide = false;
+    config.roles = false;
+  }
 
   ngOnInit(): void {
   }
