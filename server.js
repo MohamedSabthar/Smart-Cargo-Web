4701a6fe3797a1732a45485ab232dd0ecea12230
Mohamed Sabthar <sabtharugc0@gmail.com>
2020-06-28 01:59:09 +0530

    directory name case sensitvie error resolved in server.js

--- server.js
+++ server.js
@@ -2,8 +2,8 @@
 const express = require('express');
 const path = require('path');
 const ngApp = express();
-ngApp.use(express.static('./dist/smart-cargo-web'));
+ngApp.use(express.static('./dist/Smart-Cargo-Web'));
 ngApp.get('/*', function (request, response) {
-    response.sendFile(path.join(__dirname, '/dist/smart-cargo-web/index.html'));
+    response.sendFile(path.join(__dirname, '/dist/Smart-Cargo-Web/index.html'));
 });
 ngApp.listen(process.env.PORT || 8080);
