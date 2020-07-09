1bd2ab1e12c84b0bd29f5862f7ebc2fb66a9498a
Thushanthy <thushanthywalter10@gmail.com>
2020-07-09 23:04:36 +0530

    timeline component ui implemented

--- src/app/pages/main-content/admin/track-order/track-order.component.ts
+++ src/app/pages/main-content/admin/track-order/track-order.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { TrackVehileMapComponent } from 'src/app/components/track-vehile-map/track-vehile-map.component';
 import { TrackVehileDetailsComponent } from 'src/app/components/track-vehile-details/track-vehile-details.component';
+import { TimeLineComponent } from 'src/app/components/time-line/time-line.component';
 
 @Component({
   selector: 'app-track-order',
@@ -24,6 +25,16 @@ export class TrackOrderComponent implements OnInit {
 
   openDetailsModal() {
     const modalRef = this.modalService.open(TrackVehileDetailsComponent);
+    
+    modalRef.result.then((result) => {
+      console.log(result);
+    }).catch((error) => {
+      console.log(error);
+    });
+  }
+
+  openStatusModal() {
+    const modalRef = this.modalService.open(TimeLineComponent);
     
     modalRef.result.then((result) => {
       console.log(result);
