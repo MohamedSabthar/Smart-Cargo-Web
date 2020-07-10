7bcd81ee2e349a8bee9daf62c2573b8cfd44faf0
Aksshar <rameshakshar700@gmail.com>
2020-07-10 10:01:50 +0530

    schedule order ui models implemented

--- src/app/components/schedule-order-assign-driver/schedule-order-assign-driver.component.ts
+++ src/app/components/schedule-order-assign-driver/schedule-order-assign-driver.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-schedule-order-assign-driver',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ScheduleOrderAssignDriverComponent implements OnInit {
 
-  constructor() { }
+  constructor(public activeModal: NgbActiveModal) { }
+
+  closeModal() {
+    this.activeModal.close('Modal Closed');
+  }
 
   ngOnInit(): void {
   }
--- src/app/components/schedule-order-order-list/schedule-order-order-list.component.ts
+++ src/app/components/schedule-order-order-list/schedule-order-order-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-schedule-order-order-list',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ScheduleOrderOrderListComponent implements OnInit {
 
-  constructor() { }
+  constructor(public activeModal: NgbActiveModal) { }
+
+  closeModal() {
+    this.activeModal.close('Modal Closed');
+  }
 
   ngOnInit(): void {
   }
