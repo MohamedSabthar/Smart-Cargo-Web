e308b70ddfd9b661a8d1a6f66b7210f5ff4101b2
Mohamed Sabthar <sabtharugc0@gmail.com>
2020-07-12 00:25:01 +0530

    merge conflict resolved in admin.service.ts

--- src/app/services/admin.service.ts
+++ src/app/services/admin.service.ts
@@ -3,7 +3,6 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { catchError } from 'rxjs/operators';
 import { Observable, throwError } from 'rxjs';
-import { API } from '../api.constants';
 import { Storekeepers } from '../models/storekeeper.response';
 
 @Injectable({
