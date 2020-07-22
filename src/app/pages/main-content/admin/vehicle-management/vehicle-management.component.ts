831ab2fb011b5591d50e58b645f1964ef135d335
Thushanthy <thushanthywalter10@gmail.com>
2020-07-22 14:26:18 +0530

    http get method connected for vehicles

--- src/app/pages/main-content/admin/vehicle-management/vehicle-management.component.ts
+++ src/app/pages/main-content/admin/vehicle-management/vehicle-management.component.ts
@@ -1,22 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import {NgbNavConfig} from '@ng-bootstrap/ng-bootstrap';
-
+import {StoreKeeperService}from './../../../../services/store-keeper.service';
+import{VehicleDetails} from './../../../../models/vehicleDetails';
 @Component({
   selector: 'app-vehicle-management',
   templateUrl: './vehicle-management.component.html',
   styleUrls: ['./vehicle-management.component.css'],
   providers: [NgbNavConfig]
 })
+
+
 export class VehicleManagementComponent implements OnInit {
 
-
-  constructor(config: NgbNavConfig) {
+  vehicles: VehicleDetails[];
+  constructor(config: NgbNavConfig, private _storekeeperService: StoreKeeperService,) {
     // customize default values of navs used by this component tree
+
     config.destroyOnHide = false;
     config.roles = false;
   }
 
   ngOnInit(): void {
+    this._storekeeperService.getListOfVehicles().subscribe(
+      (response) => {
+        this.vehicles= response.vehicles;
+        console.log(this.vehicles);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
 
 }
