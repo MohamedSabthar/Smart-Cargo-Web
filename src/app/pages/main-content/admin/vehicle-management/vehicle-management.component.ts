9472894c00ff00e5c90249116beccdadcb45ede2
Thushanthy <thushanthywalter10@gmail.com>
2020-07-26 19:42:21 +0530

    form validation added

--- src/app/pages/main-content/admin/vehicle-management/vehicle-management.component.ts
+++ src/app/pages/main-content/admin/vehicle-management/vehicle-management.component.ts
@@ -3,6 +3,7 @@ import {NgbNavConfig} from '@ng-bootstrap/ng-bootstrap';
 import {StoreKeeperService}from './../../../../services/store-keeper.service';
 import{VehicleDetails} from './../../../../models/vehicleDetails';
 import{Vehicletype}from './../../../../models/vehicletypeDetails';
+import { FormGroup, FormControl, Validators, FormBuilder, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-vehicle-management',
@@ -17,7 +18,9 @@ export class VehicleManagementComponent implements OnInit {
   vehicles: VehicleDetails[];
   vehiclesType: Vehicletype[];
 
-  constructor(config: NgbNavConfig, private _storekeeperService: StoreKeeperService,) {
+  AddVehicleForm:FormGroup;
+  
+  constructor(config: NgbNavConfig, private _storekeeperService: StoreKeeperService,private fb: FormBuilder) {
     // customize default values of navs used by this component tree
 
     config.destroyOnHide = false;
@@ -25,6 +28,30 @@ export class VehicleManagementComponent implements OnInit {
   }
 
   ngOnInit(): void {
+
+
+//validation for add vehicle form
+    this.AddVehicleForm = this.fb.group({
+      VehicleType:  ['', [
+        Validators.required,
+        
+      ],],
+      Capacity:  ['', [
+        Validators.required,
+        
+      ],],
+
+      Load:  ['', [
+        Validators.required,
+        
+      ],],
+
+      Fueleconomy:  ['', [
+        Validators.required,
+        
+      ],],
+    });
+
     this._storekeeperService.getListOfVehicles().subscribe(
       (response) => {
         this.vehicles= response.vehicles;
@@ -34,7 +61,7 @@ export class VehicleManagementComponent implements OnInit {
         console.log(error);
       }
     );
-  
+
 
   this._storekeeperService.getListOfVehiclesTypes().subscribe(
     (response) => {
@@ -45,6 +72,21 @@ export class VehicleManagementComponent implements OnInit {
       console.log(error);
     }
   );
+}
+
+//getters for form validations
+get VehicleType() {
+  return this.AddVehicleForm.get('VehicleType')
+}
+
+get Capacity() {
+  return this.AddVehicleForm.get('Capacity')
+}
+get Load() {
+  return this.AddVehicleForm.get('Load')
+}
+get Fueleconomy() {
+  return this.AddVehicleForm.get(' Fueleconomy')
 }
 
 }
