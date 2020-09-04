1c3ad6b91ba0f740b7670702a3cfd9f6c562d94f
Mohamed Sabthar <sabtharugc0@gmail.com>
2020-09-04 17:01:38 +0530

    connected openstreetmap to display customer location

--- src/app/components/view-order-details/view-order-details.component.ts
+++ src/app/components/view-order-details/view-order-details.component.ts
@@ -1,43 +1,78 @@
 import { Orders } from './../../models/orderDetails';
 import { Component, OnInit, Input } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
-import {DomSanitizer} from '@angular/platform-browser';
 
+declare var ol: any;
 
 @Component({
   selector: 'app-view-order-details',
   templateUrl: './view-order-details.component.html',
-  styleUrls: ['./view-order-details.component.css']
+  styleUrls: ['./view-order-details.component.css'],
 })
 export class ViewOrderDetailsComponent implements OnInit {
+  @Input() public order: Orders;
 
-  @Input() public order:Orders;
-  mapLocation:string;
 
-  constructor(private _activeModal: NgbActiveModal,private _sanitizer:DomSanitizer) {
+  constructor(private _activeModal: NgbActiveModal) {}
 
-  }
+  latitude: Number
+  longitude: Number
 
+  map: any;
 
   ngOnInit(): void {
-    console.log(this.order)
-    this.mapLocation = "https://www.google.com/maps/embed/v1/place?key=AIzaSyDQMuWTo1cJ8s3pm0ZbbOi65jB8E4-LJVE&q="+this.order.location.lat+','+this.order.location.lang;
+
+    this.latitude = this.order.location.lat;
+    this.longitude = this.order.location.lang;
+
+    var attribution = new ol.control.Attribution({
+      collapsible: false,
+    });
+
+    console.log(this.order.location)
+    //add map
+    this.map = new ol.Map({
+      target: 'map',
+      layers: [
+        new ol.layer.Tile({
+          source: new ol.source.OSM()
+        })
+      ],
+      view: new ol.View({
+        center: ol.proj.fromLonLat([ this.longitude,this.latitude]),
+        zoom: 16
+      })
+    });
+
+    //add marker
+    var layer = new ol.layer.Vector({
+      source: new ol.source.Vector({
+        features: [
+          new ol.Feature({
+            geometry: new ol.geom.Point(
+              ol.proj.fromLonLat([this.longitude,this.latitude])
+            ),
+            color: 'red',
+          }),
+        ],
+      }),
+    });
+
+    this.map.addLayer(layer);
   }
 
   closeModal() {
     this._activeModal.close('close');
   }
 
-   getEmergencyLevel():string{
-    switch(this.order.emergency_level){
-      case 1: return "High"
-      case 2: return "Medium"
-      case 3: return "Low"
+  getEmergencyLevel(): string {
+    switch (this.order.emergency_level) {
+      case 1:
+        return 'High';
+      case 2:
+        return 'Medium';
+      case 3:
+        return 'Low';
     }
   }
-
-  getLocationOnMap(){
-    return this._sanitizer.bypassSecurityTrustResourceUrl(this.mapLocation);
-  }
-
 }
