3d8b5884b8391e0aa87322bfef7608e778229ac2
Thushanthy <thushanthywalter10@gmail.com>
2020-10-04 21:13:55 +0530

    open layers map inserted

--- src/app/components/urgency-level/urgency-level.component.ts
+++ src/app/components/urgency-level/urgency-level.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var ol: any;
 @Component({
   selector: 'app-urgency-level',
   templateUrl: './urgency-level.component.html',
@@ -7,9 +8,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UrgencyLevelComponent implements OnInit {
   valueChange = false;
+  map: any;
   constructor() { }
 
   ngOnInit(): void {
+
+    var attribution = new ol.control.Attribution({
+      collapsible: false,
+    });
+
+     //add map
+     this.map = new ol.Map({
+      target: 'map',
+      layers: [
+        new ol.layer.Tile({
+          source: new ol.source.OSM()
+        })
+      ],
+      view: new ol.View({
+        center: ol.proj.fromLonLat([ 79.859936, 6.879386]),
+        zoom: 12
+      })
+    });
+
+
+   
+        //add marker
+        var layer0 = new ol.layer.Vector({
+          source: new ol.source.Vector({
+            features: [
+              new ol.Feature({
+                geometry: new ol.geom.Point(
+                  ol.proj.fromLonLat([79.859936, 6.879386])
+                ),
+                color: "red",
+              }),
+            ],
+          }),
+        });
+        this.map.addLayer(layer0);
+
+
+
+
   }
 
   onValueChange(value: boolean) {
