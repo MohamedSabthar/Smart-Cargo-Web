02b2586f687d4c4fdd45f3a547047b827510c20b
Mohamed Sabthar <sabtharugc0@gmail.com>
2020-10-12 11:43:57 +0530

    sidebar componenet modified

--- src/app/components/side-bar/side-bar.component.ts
+++ src/app/components/side-bar/side-bar.component.ts
@@ -1,3 +1,4 @@
+import { StoreKeeperService } from 'src/app/services/store-keeper.service';
 import { AuthService } from './../../services/auth.service';
 import { Component, OnInit } from '@angular/core';
 
@@ -8,11 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SideBarComponent implements OnInit {
   role: String;
+  name:String;
 
-  constructor(private _authService: AuthService) {}
+  constructor(private _authService: AuthService,private _storeKeeperService:StoreKeeperService) {}
 
   ngOnInit(): void {
     this.role = this._authService.getRole();
+    this._storeKeeperService.getProfile(this._authService.getId()).subscribe((res)=>{
+      console.log('profile')
+      this.name = `${res.result.name.first}`
+    },(error)=>{
+      this.name = "Loading..."
+    })
   }
 
   //logout from the application
